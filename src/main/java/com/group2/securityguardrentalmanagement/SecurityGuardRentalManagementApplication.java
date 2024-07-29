f8d1aad3f6f3d8513d1a302509fb3fbcf173640b
ninhtvo <124490185+ninhtvo@users.noreply.github.com>
2024-07-29 22:11:17 +0700

    Update SecurityGuardRentalManagementApplication.java

--- src/main/java/com/group2/securityguardrentalmanagement/SecurityGuardRentalManagementApplication.java
+++ src/main/java/com/group2/securityguardrentalmanagement/SecurityGuardRentalManagementApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class SecurityGuardRentalManagementApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SecurityGuardRentalManagementApplication.class, args);
-	}
+		pringApplication.run(SecurityGuardRentalManagementApplication.class, args);
+	
 
 }
